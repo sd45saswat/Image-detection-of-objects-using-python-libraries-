71d6dccc3784cc2985c8fb300a68236c599ebbcb
Saswat Singh <saswatrs251104@gmail.com>
2024-03-15 22:13:23 +0530

    Update staticimage.py

--- staticimage.py
+++ staticimage.py
@@ -1,60 +1,60 @@
 import tensorflow_hub as hub
 import cv2
-import numpy
+import numpy as np
 import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
+
 # Apply image detector on a batch of image.
 detector = hub.load("https://tfhub.dev/tensorflow/efficientdet/lite2/detection/1")
 width = 1028
 height = 1028
 
-#Load image by Opencv2
-img = cv2.imread('image.jpg')
-#Resize to respect the input_shape
+# Load image using OpenCV
+img = cv2.imread('Image.jpg.jpeg')
+
+# Resize the image to match the input shape
 inp = cv2.resize(img, (width , height ))
 
-#Convert img to RGB
+# Convert the image to RGB
 rgb = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
 
-#Is optional but i recommend (float convertion and convert img to tensor image)
+# Convert the RGB image to a TensorFlow tensor
 rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
 
-#Add dims to rgb_tensor
+# Add a batch dimension to the tensor
 rgb_tensor = tf.expand_dims(rgb_tensor , 0)
 
-plt.figure(figsize=(10,10))
-plt.imshow(rgb)
-<matplotlib.image.AxesImage at 0x7f224ac415d0>
-
+# Perform object detection on the image
 boxes, scores, classes, num_detections = detector(rgb_tensor)
-labels = pd.read_csv('labels.csv',sep=';',index_col='ID')
+
+# Load object labels from a CSV file
+labels = pd.read_csv('labels.csv', sep=';', index_col='ID')
 labels = labels['OBJECT (2017 REL.)']
-labels.head()
-ID
-1        person
-2       bicycle
-3           car
-4    motorcycle
-5      airplane
-Name: OBJECT (2017 REL.), dtype: object
-pred_labels = classes.numpy().astype('int')[0] 
+
+# Extract predicted labels, boxes, and scores
+pred_labels = classes.numpy().astype('int')[0]
 pred_labels = [labels[i] for i in pred_labels]
 pred_boxes = boxes.numpy()[0].astype('int')
 pred_scores = scores.numpy()[0]
 
-for score, (ymin,xmin,ymax,xmax), label in zip(pred_scores, pred_boxes, pred_labels):
-        if score < 0.5:
-            continue
-            
-        score_txt = f'{100 * round(score)}%'
-        img_boxes = cv2.rectangle(rgb,(xmin, ymax),(xmax, ymin),(0,255,0),2)      
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img_boxes, label,(xmin, ymax-10), font, 1.5, (255,0,0), 2, cv2.LINE_AA)
-        cv2.putText(img_boxes,score_txt,(xmax, ymax-10), font, 1.5, (255,0,0), 2, cv2.LINE_AA)
+# Draw bounding boxes and labels on the image
+for score, (ymin, xmin, ymax, xmax), label in zip(pred_scores, pred_boxes, pred_labels):
+    if score < 0.5:
+        continue
+        
+    score_txt = f'{100 * round(score)}%'
+    rgb = cv2.rectangle(rgb,(xmin, ymax),(xmax, ymin),(0,255,0),2)      
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(rgb, label, (xmin, ymax-10), font, 1.5, (255,0,0), 2, cv2.LINE_AA)
+    cv2.putText(rgb, score_txt, (xmax, ymax-10), font, 1.5, (255,0,0), 2, cv2.LINE_AA)
+
+# Plot the image with bounding boxes and labels
 plt.figure(figsize=(10,10))
-plt.imshow(img_boxes)
+plt.imshow(rgb)
 
-plt.savefig('image_pred.jpg',transparent=True, )
+# Save the plotted image
+plt.savefig('image_pred.jpg', transparent=True)
 
-
+# Show the plot
+plt.show()
